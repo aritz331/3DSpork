ef1dad51b70a4b3ef4bd669ac67a1973566d0518
stivengiv <94831607+stivengiv@users.noreply.github.com>
2023-03-10 19:27:08 +0100

    w

--- script.js
+++ script.js
@@ -17,3 +17,4 @@ const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
 const createSubtitle = text => text.split("  ").map(addWord);
 
 createSubtitle("Download  the  spork  3D  model  <a href='Sporknife.zip' style='text-decoration: none;' download>here</a>.");
+createSubtitle("The  idea  of  the  3D  spork  knife  was  originated  from  a  writing  test.");
